e2d1db2e6b7a84942eed29553418d9c36334ce9c
Rishav <reshav01@gmail.com>
2026-02-14 22:45:43 +0100

    changes

--- hello.c
+++ hello.c
@@ -2,11 +2,13 @@
 #include "user.h"
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < 10; ++i) {
-    int readcount = getreadcount();
-    printf(1, "readcount: %d\n", readcount);
-    int buf[1];
-    read(0, &buf, 0);
-  }
+  // for (int i = 0; i < 10; ++i) {
+  //   int readcount = getreadcount();
+  //   printf(1, "readcount: %d\n", readcount);
+  //   int buf[1];
+  //   read(0, &buf, 0);
+  // }
+  char *p = 0;
+  printf(1, "%x\n", (unsigned int)*p);
   exit();
 }
